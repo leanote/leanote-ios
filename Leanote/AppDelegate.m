f92c0efdaac8aa7e57badeb14a539105b1e1dc4d
Wong Zigii <wongzigii@outlook.com>
2016-04-28 16:33:36 +0800

    Replace deprecated methods in iOS 7 with NSForegroundColorAttributeName and NSShadowAttributeName

--- Leanote/AppDelegate.m
+++ Leanote/AppDelegate.m
@@ -197,16 +197,16 @@
 	
 	
 	// uisearchbar的cancel文字
-	[[UIBarButtonItem appearanceWhenContainedIn:[UISearchBar class], nil]
-	 setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-																								  [WPStyleGuide wordPressBlue],
-																								  UITextAttributeTextColor,
-																								  [UIColor clearColor],
-																								  UITextAttributeTextShadowColor,
-																								  [NSValue valueWithUIOffset:UIOffsetMake(0, -1)],
-																								  UITextAttributeTextShadowOffset,
-																								  nil]
-																						forState:UIControlStateNormal];
+    
+    NSShadow *shadow = [NSShadow new];
+    [shadow setShadowColor:[UIColor clearColor]];
+    [shadow setShadowOffset:CGSizeMake(0.0f, -1.0f)];
+    UIBarButtonItem *barButtonItem = [UIBarButtonItem appearanceWhenContainedInInstancesOfClasses:@[[UISearchBar class]]];
+    [barButtonItem setTitleTextAttributes:@{
+                                            NSForegroundColorAttributeName: [WPStyleGuide wordPressBlue],
+                                            NSShadowAttributeName: shadow,
+                                            }
+                                 forState:UIControlStateNormal];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
